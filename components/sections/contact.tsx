096dcf0488feaf436386dc287441decad79e1c35
Ahamed Shahmi <124166029+shahmi0519@users.noreply.github.com>
2025-05-31 21:38:08 +0530

    Update contact.tsx

--- components/sections/contact.tsx
+++ components/sections/contact.tsx
@@ -12,7 +12,7 @@ import { Label } from "@/components/ui/label"
 import { useToast } from "@/hooks/use-toast"
 import { Mail, Phone, MapPin, Send, Github, Linkedin, Loader2 } from "lucide-react"
 
-import emailjs from '@emailjs/browser';
+// import emailjs from '@emailjs/browser';
 
 export function Contact() {
   const [isSubmitting, setIsSubmitting] = useState(false)
